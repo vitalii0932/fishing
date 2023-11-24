a00f60c30878633ea120a0c313c3e1b999ceab6a
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-11-24 17:45:49 +0200

    Create ShoppingCart.java

---
+++ src/main/java/com/example/ookp/model/ShoppingCart.java
@@ -0,0 +1,18 @@
+package com.example.ookp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Entity(name = "shopping_cart")
+@Component
+@Data
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int[] products;
+    @Column(name = "total_price")
+    private double totalPrice;
+    private String status;
+}
