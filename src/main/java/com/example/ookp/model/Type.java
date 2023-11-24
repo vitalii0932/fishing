3c76641b6a34f59fb2f18622149ec5c2c1c4cb9f
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-11-24 17:45:55 +0200

    Create Type.java

---
+++ src/main/java/com/example/ookp/model/Type.java
@@ -0,0 +1,15 @@
+package com.example.ookp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity(name = "type")
+@Data
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @ManyToOne
+    private Category category;
+}
