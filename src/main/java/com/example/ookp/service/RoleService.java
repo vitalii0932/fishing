5316c2e47ed8ac0e576cc4219bbc0089bf0412cd
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-11-24 17:46:27 +0200

    Create RoleService.java

---
+++ src/main/java/com/example/ookp/service/RoleService.java
@@ -0,0 +1,45 @@
+package com.example.ookp.service;
+
+import com.example.ookp.dto.RoleDTO;
+import com.example.ookp.mapper.RoleMapper;
+import com.example.ookp.model.Role;
+import com.example.ookp.repository.RoleRepository;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Data
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    @Transactional(readOnly = true)
+    public List<Role> getAll() {
+        return roleRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Role findById(int id) {
+        return roleRepository.findById(id).get();
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    @Retryable(maxAttempts = 5)
+    public void add(RoleDTO roleDTO) {
+        roleRepository.save(roleMapper.toRole(roleDTO));
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    @Retryable(maxAttempts = 5)
+    public void deleteById(int id) {
+        roleRepository.deleteById(id);
+    }
+}
