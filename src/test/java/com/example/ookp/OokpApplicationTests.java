3fd6ce3df18e6c9762b97b3f7ba7ebdde4dcc317
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-11-24 17:49:46 +0200

    Create OokpApplicationTests.java

---
+++ src/test/java/com/example/ookp/OokpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ookp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OokpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
