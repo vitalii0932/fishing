53c51559c4fb371259a3335138640dfbf6b0fc73
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-11-24 17:45:15 +0200

    Create ShoppingCartDTO.java

---
+++ src/main/java/com/example/ookp/dto/ShoppingCartDTO.java
@@ -0,0 +1,13 @@
+package com.example.ookp.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ShoppingCartDTO {
+    private int id;
+    private List<Integer> productsIds;
+    private double totalPrice;
+    private String status;
+}
