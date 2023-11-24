222e3d0fa47be8018c73e758f1221943afc5d7f3
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-11-24 17:46:02 +0200

    Create OokpApplication.java

---
+++ src/main/java/com/example/ookp/OokpApplication.java
@@ -0,0 +1,13 @@
+package com.example.ookp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OokpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OokpApplication.class, args);
+    }
+
+}
